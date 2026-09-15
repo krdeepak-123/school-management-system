1baa047b11108c233ce2c8826384ba62a34a6e6b
Deepak kumar <deepakkumar23666@gmail.com>
2026-09-15 13:27:24 +0530

    check it

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
 import MainLayout from "../layouts/MainLayout";
-
 import Dashboard from "../pages/Dashboard";
 import Students from "../pages/Students";
 import Teachers from "../pages/Teachers";
